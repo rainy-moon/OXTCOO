d32500de0318587dc0758d3363761a16e585e499
rain <zhangyucheng0505@126.com>
2023-04-04 22:26:07 +0800

    类定义和初始化

--- OXTCOO.cpp
+++ OXTCOO.cpp
@@ -1,18 +1,18 @@
 ﻿#include<stdio.h>
 #include<vector>
+#include<iostream>
 using namespace std;
-
-int P = 20;
-int N, M, T, D;
-
+int P;//任务数量
+int N, M, T, D; //节点数量、管道数量、信道数量、衰减距离
+int E = 0; //总边数
 
 class pipe {
 public:
 	int id;
 	int cost;
-	int* channel = new int[P];
+	int channel[80];
 	pipe() {}
-	pipe(int id, int cost) {
+	void initpipe(int id, int cost) {
 		this->id = id;
 		this->cost = cost;
 	}
@@ -23,7 +23,7 @@ public:
 	int nodeNo2;
 	vector<int> p;
 	edge() {}
-	edge(int nodeNo1, int nodeNo2) {
+	void initedge(int nodeNo1, int nodeNo2) {
 		this->nodeNo1 = nodeNo1;
 		this->nodeNo2 = nodeNo2;
 	};
@@ -32,38 +32,69 @@ public:
 		return this->nodeNo1 == node1 ? this->nodeNo2 : this->nodeNo1;
 	}
 };
+edge edges[5005];
 class task {
 public:
 	int id;
 	int from;
 	int to;
 	int power;
-	bool* possibleChannel = new bool[P];
+	bool possibleChannel[80];
 	task() {}
-	task(int id, int from, int to, int power) {
+	void inittask(int id, int from, int to, int power) {
 		this->id = id;
 		this->from = from;
 		this->to = to;
 		this->power = power;
 	}
 };
+task tasks[10005];
 class node {
 public:
 	int id;
 	vector<int> e;
 	node() {}
-	node(int id) {
+	void initnode(int id) {
 		this->id = id;
 	};
-
+	int getEdge(int dst) {
+		for (int i = 0; i < e.size(); i++) {
+			int edgeId = e[i];
+			if (edges[edgeId].another(id) == dst) {
+				return edgeId;
+			}
+		}
+		return -1;
+	}
 };
-
-edge edges[5005];
-vector<pipe> pipes;
-task tasks[10005];
 node nodes[5005];
+vector<pipe> pipes;
+
+void initData() {
+	scanf("%d %d %d %d %d", &N, &M, &T, &P, &D);
+	for (int i = 0; i < N; i++) {
+		nodes[i].initnode(i);
+	}
+	pipes.resize(M);
+	for (int i = 0; i < M; i++) {
+		int si, ti, di;
+		scanf("%d %d %d", &si, &ti, &di);
+		pipes[i].initpipe(i, di);
+		if (nodes[si].getEdge(ti) == -1) {
+			edges[E].initedge(si, ti);
+			nodes[si].e.push_back(E);
+			nodes[ti].e.push_back(E++);
+		}
+	}
+	for (int i = 0; i < T; i++) {
+		int sj, tj;
+		scanf("%d %d", &sj, &tj);
+		tasks[i].inittask(i, sj, tj,D);
+	}
+}
 
 int main() {
-	printf("hello world");
+	initData();
+	cout << ("hello world");
 }
 
